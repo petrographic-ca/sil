f10694b3dd5ac42d7571f5f031e88e7a6f55a3ca
edoules <ema@uoguelph.ca>
2022-08-11 08:03:20 -0400

    feat(bricks): start bricks cli code and lib

--- ImageJ/plugins/CTofSoil/ca/petrographic/SilLibrary.java
+++ ImageJ/plugins/CTofSoil/ca/petrographic/SilLibrary.java
@@ -23,4 +23,24 @@ public class SilLibrary {
         outPlus.setDisplayRange(0, outPlus.getStack().getSize() -1);
         return outPlus;
     }
+
+    static public ImagePlus makeBricks(
+            int width, int height, int depth,
+            int brick_width, int brick_height, int brick_depth,
+            int margin_thickness, int stick_thickness) {
+        ImageStack outStack = new ImageStack(width, height);
+        for(int islice = 1; islice <= depth; islice ++) {
+            int[] raw_data = new int[width * height];
+
+            Arrays.fill(raw_data, islice);  // add bricking logic here
+
+            outStack.addSlice(new FloatProcessor(width, height, raw_data));
+        }
+        ImagePlus outPlus = new ImagePlus(
+            "bricks_" + outStack.getWidth() + "_"
+            + outStack.getHeight() + "_" + outStack.getSize(),
+            outStack);
+        outPlus.setDisplayRange(0, outPlus.getStack().getSize() -1);
+        return outPlus;
+    }
 }
